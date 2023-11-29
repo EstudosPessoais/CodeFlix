bfd9cac08ce52a3db26a01bcd2273ca1ed603eb3
bielb1b2 <gabrielvt.souza@hotmail.com>
2023-11-29 13:35:34 -0300

    Adding: Update Category

---
+++ FC.Codeflix.Catalog/tests/FC.Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -0,0 +1,70 @@
+﻿using FC.Codeflix.Catalog.Application.Interfaces;
+using FC.Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+using FC.Codeflix.Catalog.Domain.Entity;
+using FC.Codeflix.Catalog.Domain.Repository;
+using FC.Codeflix.Catalog.UnitTests.common;
+using Moq;
+
+namespace FC.Codeflix.Catalog.UnitTests.Application.UpdateCategory
+{
+    [CollectionDefinition(nameof(UpdateCategoryTestFixture))]
+    public class UpdateCategoryTestFixtureCollection : ICollectionFixture<UpdateCategoryTestFixture> {};
+
+    public class UpdateCategoryTestFixture : BaseFixture
+    {
+        public Mock<ICategoryRespository> GetRepositoryMock()
+        {
+            return new Mock<ICategoryRespository>(); ;
+        }
+
+        public Mock<IUnitOfWork> GetUnitOfWorkMock()
+        {
+            return new Mock<IUnitOfWork>();
+        }
+
+        public string GetValidCategoryName()
+        {
+            var categoryName = "";
+            while (categoryName.Length < 3)
+                categoryName = Faker.Commerce.Categories(1)[0];
+            if (categoryName.Length > 255)
+                categoryName = categoryName[..255];
+            return categoryName;
+        }
+
+        public string GetValidCategoryDescription()
+        {
+            var categoryDescription = Faker.Commerce.ProductDescription();
+            if (categoryDescription.Length > 10000)
+                categoryDescription = categoryDescription[..10000];
+            return categoryDescription;
+        }
+
+        public bool GetRandomBoolean()
+        {
+            return (new Random()).NextDouble() < 0.5;
+        }
+
+        public CreateCategoryInput GetInput()
+            => new(
+                    GetValidCategoryName(),
+                    GetValidCategoryDescription(),
+                    GetRandomBoolean()
+                   );
+
+        public Category GetExampleCategory()
+            => new Category(GetValidCategoryName(), GetValidCategoryDescription(), GetRandomBoolean());
+
+        public UpdateCategoryInput GetValidInput(Guid? id = null)
+        {
+            return new UpdateCategoryInput(
+                   id ?? Guid.NewGuid(),
+                   GetValidCategoryName(),
+                   GetValidCategoryDescription(),
+                   GetRandomBoolean()
+               );
+        }
+
+    }
+}
